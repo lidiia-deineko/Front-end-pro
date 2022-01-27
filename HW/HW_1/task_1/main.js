b5de9068f595efecd7bb88e11a0c97c00c9947de
lidiia-deineko <67641622+lidiia-deineko@users.noreply.github.com>
2022-01-27 16:03:09 +0200

    Add files via upload

--- HW/HW_1/task_1/main.js
+++ HW/HW_1/task_1/main.js
@@ -1,17 +1,19 @@
 let radius = +prompt('Введите радиус цилиндра: ');
 let height = +prompt('Введите высоту цилиндра: ');
 
-let result = Math.PI * radius**2 * height;
+let line = '--------------------';
+let areaCylinder = Math.PI * radius**2;
+let volumeCylinder = areaCylinder * height;
 
 document.write('**************<br>');
 
-document.write(`Обьем цилиндра с площадью основы *${Math.PI * radius*radius}*, радиусом *${radius}* и высотой *${height}* равен:<br>`);
+document.write(`Обьем цилиндра с площадью основы *${areaCylinder}*, радиусом *${radius}* и высотой *${height}* равен:<br>`);
 
-document.write('--------------------<br>');
+document.write(`${line}<br>`);
 
 document.write(`V = ${result}.<br>`);
 
-document.write('--------------------<br>');
+document.write(`${line}<br>`);
 
 document.write('end.');
 
