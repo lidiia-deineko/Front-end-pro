22aedc32c730364ee1bf6155aa3e68a9932e7616
lidiia-deineko <67641622+lidiia-deineko@users.noreply.github.com>
2022-01-27 16:23:01 +0200

    Add files via upload

--- HW/HW_1/task_2/main.js
+++ HW/HW_1/task_2/main.js
@@ -1,18 +1,19 @@
-let varA = +prompt('Введите переменную a: ');
-let varB = +prompt('Введите переменную b: ');
-let varC = +prompt('Введите переменную c: ');
+let nameRequest = 'Введите переменную';
+let statusVar = 'переменная четная';
+let varA = +prompt(`${nameRequest} a: `);
+let varB = +prompt(`${nameRequest} b: `);
+let varC = +prompt(`${nameRequest} c: `);
 
 let sumVar = varA+varB+varC;
 document.write(`Сумма переменных - ${sumVar}<br>`);
 
 if(varA%2 == 0){
-    document.write('Переменная а - четная.<br>')
+    document.write(`a - ${statusVar}<br>`);
 }
-
 if(varB%2 == 0){
-    document.write('Переменная b - четная.<br>')
+    document.write(`b - ${statusVar}<br>`);
+}
+if(varC%2 == 0){
+    document.write(`c - ${statusVar}<br>`);
 }
 
-if(varC%2 == 0){
-    document.write('Переменная c - четная.')
-}
